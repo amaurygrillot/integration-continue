31a8b67c23707ec0c2ac744b3dd7f669ad049f09
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2022-03-07 03:13:41 +0100

    copy server.js to app directory

---
+++ src/app/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('./dist/PokemonArena'));
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {root : 'dist/PokemonArena'});
+});
+app.listen(process.env.PORT || 8080);
