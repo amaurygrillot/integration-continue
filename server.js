bd7a62ad59c9eb3afabc8d87b17874746236d39a
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2022-03-07 03:07:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static('./dist/PokemonArena'));
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile('index.html', {root : 'dist/PokemonArena'});
 });
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
