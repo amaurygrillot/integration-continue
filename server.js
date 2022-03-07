e3bf02f43cc520f73ef311adbd3c07e4a062f4ad
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2022-03-07 03:00:35 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}const express = require('express');
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static('./dist/<name-on-package.json>'));
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-);
+app.use(express.static('./dist/PokemonArena'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', {root : 'dist/PokemonArena'});
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
